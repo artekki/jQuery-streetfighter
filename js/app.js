da39e98a3d8c0234a89117c81ff3fb1f7059863e
Raphael Shin <djrpy4all@gmail.com>
2015-08-10 16:22:01 -0700

    jqueryerror

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-$(document).ready(fuction() { 
-	$('.ryu').mouseenter(function() {
-		alert('mouse entered .ryu div');
+$(document).ready(fuction(){ 
+	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	.mouseleave(function() {
-		alert('mouse left');
-	});
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show.animate(
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
+
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
 });
+
