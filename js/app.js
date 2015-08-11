4bd94e7b27d23885424acc7a51fe80d4098c873c
Raphael Shin <djrpy4all@gmail.com>
2015-08-11 16:48:22 -0700

    done but error

--- js/app.js
+++ js/app.js
@@ -1,27 +1,54 @@
-$(document).ready(fuction(){ 
-	$('.ryu').mouseenter(function(){
+$(document).ready(function() { 
+	
+	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	.mouseleave(function(){
+	
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
-	.mousedown(function(){
+	
+	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show.animate(
+		$('.hadouken').finish().show()
+		.animate(
 			{'left': '1020px'},
-			500,
-			function(){
+			600,
+			function() {
+				$(this).stop();
 				$(this).hide();
 				$(this).css('left', '520px');
 			});
-
 	})
+	
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
-});
 
+	$(document).keydown(function(e) {
+		if(e.which == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		};
+	})
+
+	$(document).keyup(function(e) {
+		if(e.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show;
+		};
+	})
+
+})
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
